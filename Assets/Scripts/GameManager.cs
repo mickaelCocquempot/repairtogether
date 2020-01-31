86ef63229406f1b38c8fade19e4e0974ca8897cc
Ulysse Vautier <ulysse.vautier@gmail.com>
2020-01-31 22:58:50 +0100

    First GameManager with different actions and inputs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<IActions> actions = new List<IActions>();
+    public List<IUsersInput> users = new List<IUsersInput>();
+
+    public int usersN = 1;
+
+    void Start()
+    {
+        actions.Add(new HorizontalAction());
+        for(int i = 0; i < usersN; ++i)
+        {
+            addPlayer();
+        }
+    }
+
+    public void setAction(IActions action, int nUser)
+    {
+        if(nUser <= usersN)
+        {
+            users[nUser].action = action;
+        }
+    }
+
+    public void addPlayer()
+    {
+        users.Add(new IUsersInput());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (IUsersInput user in users)
+        {
+            if(Input.GetAxis(user.prefix + "Trigger_LT") > 0.5f)
+            {
+                user.action = actions[(actions.IndexOf(user.action) + 1) % actions.Count];
+            }
+            else
+            {
+                user.action = actions[(actions.IndexOf(user.action) - 1) % actions.Count];
+            }
+            user.act();
+        }
+    }
+}
---
+++ Assets/Scripts/IUsersInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IUsersInput
+{
+    public static int nUsers = 0;
+    public string prefix = "";
+
+    private int nActual = 0;
+
+    public IActions action = null;
+
+    public IUsersInput(){
+        nUsers++;
+        nActual = nUsers;
+        prefix = "P" + nActual + "_";
+    }
+
+    public void act()
+    {
+        action.action(prefix, null);
+    }
+}
