41118cce21bd365e71c5d7ed619ed5801a3042d7
terag <victor@rouquette.me>
2020-02-01 15:34:12 +0100

    project update

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
@@ -75,7 +76,7 @@ public class GameManager : MonoBehaviour
     {
         foreach (IUsersInput user in users)
         {
-            //Debug.Log("Trigger_LT " + Input.GetAxis(user.prefix + "Trigger_LT"));
+            Debug.Log(user.prefix + "  " + user.GetHorizontal() + "  " + user.GetVertical() + "  " + user.GetRT() + "  " + user.GetLT());
             if (user.GetLT() > 0.5f && user.triggerLT == false)
             {
                 user.action.actionNull(obj);
