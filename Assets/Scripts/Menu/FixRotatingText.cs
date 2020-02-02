008c96bc72332b720e040a88d8ef28cd13a3c2ce
terag <victor@rouquette.me>
2020-02-02 14:57:15 +0100

    menus polish

---
+++ Assets/Scripts/Menu/FixRotatingText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixRotatingText : MonoBehaviour
+{
+	public float speed;
+	public Vector3 angles;
+	public float time = 0.5f;
+	public float chrono;
+
+	// Use this for initialization
+	void Start()
+	{
+		this.angles = transform.eulerAngles;
+		this.chrono = 0.0f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		angles.z += speed * Time.deltaTime;
+		transform.eulerAngles = angles;
+		this.chrono += Time.deltaTime;
+		if (this.chrono > this.time)
+		{
+			this.chrono -= this.time;
+			speed = -speed;
+		}
+	}
+}
---
+++ Assets/Scripts/Menu/RotatingText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingText : MonoBehaviour
+{
+	public float range;
+	public Vector3 angles;
+	public float time = 0.5f;
+	public float chrono;
+
+	// Use this for initialization
+	void Start()
+	{
+		this.angles = transform.eulerAngles;
+		this.chrono = 0.0f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		this.chrono += Time.deltaTime;
+		if (this.chrono > this.time)
+		{
+			this.chrono -= this.time;
+
+			Vector3 newRotation = this.angles;
+			angles.z += Random.Range(-1.0f, 1.0f) * range;
+
+			transform.eulerAngles = angles;
+		}
+	}
+}
