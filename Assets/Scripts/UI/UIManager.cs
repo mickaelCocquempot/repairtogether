83aa208b107f8670062c07d7b5a6579dde3e53c3
bikbar <m.cocquempot@gmail.com>
2020-02-02 01:01:29 +0100

    uimanager icon

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -24,9 +24,29 @@ public class UIManager : MonoBehaviour
     private List<GameObject> mListPlayer = new List<GameObject>();
 
     [SerializeField]
-    private List<Sprite> ListIcon = new List<Sprite>();
+    private List<GameObject> ListIcon = new List<GameObject>();
 
     private List<string> mListAction = new List<string>();
+
+    [SerializeField]
+    private GameObject PlayerOneBgOne;
+    [SerializeField]
+    private GameObject PlayerOneBgTwo;
+
+    [SerializeField]
+    private GameObject PlayerTwoBgOne;
+    [SerializeField]
+    private GameObject PlayerTwoBgTwo;
+
+    [SerializeField]
+    private GameObject PlayerThreeBgOne;
+    [SerializeField]
+    private GameObject PlayerThreeBgTwo;
+
+    [SerializeField]
+    private GameObject PlayerFourBgOne;
+    [SerializeField]
+    private GameObject PlayerFourBgTwo;
 
     private int mNbPlayer;
 
@@ -42,11 +62,14 @@ public class UIManager : MonoBehaviour
         else if (instance != this)
             Destroy(gameObject);
         DontDestroyOnLoad(gameObject);
+
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        Dictionary<string, GameObject> mDictionnaryIcon = new Dictionary<string, GameObject>();
         OpaqueScreen.SetActive(true);
         Counter.SetActive(true);
         Timer += 0.9F;
@@ -56,21 +79,43 @@ public class UIManager : MonoBehaviour
         mListPlayer.Add(Player4);
         mNbPlayer = GameManager.instance.usersN;
 
-        if(GameManager.instance.gameMode.MODE == GameMode.GAMEMODE.G4COLLABSTACK)
+        mDictionnaryIcon.Add("Horizontal", ListIcon[1]);
+        mDictionnaryIcon.Add("Vertical", ListIcon[4]);
+        mDictionnaryIcon.Add("OrientationY", ListIcon[3]);
+        mDictionnaryIcon.Add("OrientationX", ListIcon[2]);
+        mDictionnaryIcon.Add("Camera", ListIcon[5]);
+        mDictionnaryIcon.Add("Depth", ListIcon[0]);
+
+
+        for (int i = 0; i < GameManager.instance.usersN; ++i)
+        {
+            mListPlayer[i].SetActive(true);
+        }
+
+        if (GameManager.instance.gameMode.MODE == GameMode.GAMEMODE.G4COLLABSTACK)
         {
             for (int i = 0; i < GameManager.instance.usersN; ++i)
             {
+                if(i == 0)
+
                 mListPlayer[i].GetComponentInChildren<Image>().color = GameManager.instance.users[i].color;
+                Debug.Log(GameManager.instance.users[i].color.ToString());
                 for (int j = 0; j < ((GameMode.GCollabStack)GameManager.instance.gameMode).nbAction; ++j)
                 {
+
                     mListAction.Add(((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions[i][j].name);
-                    Debug.Log("Player " + i + " : " + ((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions[i][j].name);
+                    //Instantiate(mDictionnaryIcon[((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions[i][j].name], mListPlayer[i].transform.position + new Vector3(100F, 0F, 0F), Quaternion.identity, mListPlayer[i].transform);
+                    if(i == 0)
+                    {
+                        PlayerOneBgOne.GetComponentInChildren<SpriteRenderer>().sprite = mDictionnaryIcon[((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions[i][j].name].GetComponent<SpriteRenderer>().sprite;
+                        PlayerOneBgTwo.GetComponentInChildren<SpriteRenderer>().sprite = mDictionnaryIcon[((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions[i][j].name].GetComponent<SpriteRenderer>().sprite;
+                        Debug.Log(mDictionnaryIcon[((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions[i][j].name].GetComponent<SpriteRenderer>().sprite.ToString());
+                    }
+                    Debug.Log(mDictionnaryIcon[((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions[i][j].name].ToString());
+                    //Debug.LogWarning("Player " + i + " : " + ((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions[i][j].name);
                 }
             }
         }
-        
-           // ((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions;
-        //((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions;
     }
 
     // Update is called once per frame
@@ -93,7 +138,20 @@ public class UIManager : MonoBehaviour
             for (int i = 0; i < mNbPlayer; ++i)
                 mListPlayer[i].SetActive(true);
         }
-        ShowPlayerUI();
+
+        if(GameManager.instance.gameMode.MODE == GameMode.GAMEMODE.G4COLLAB)
+            ShowPlayerUI();
+        if (GameManager.instance.gameMode.MODE == GameMode.GAMEMODE.G4COLLABSTACK)
+        {
+            for (int i = 0; i < GameManager.instance.usersN; ++i)
+            {
+                for (int j = 0; j < ((GameMode.GCollabStack)GameManager.instance.gameMode).nbAction; ++j)
+                {
+                    mListAction.Add(((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions[i][j].name);
+                    Debug.Log(((GameMode.GCollabStack)GameManager.instance.gameMode).nextActions[i][j].name);
+                }
+            }
+        }
 
         TimerScore.GetComponent<Text>().text = GameManager.instance.time.ToString("F2");
     }
